2df563c10209ac3f6577001282395274b903a887
Frank Doyle <fdoyle@udel.edu>
2021-07-07 10:57:44 -0400

    Update to same form as the used in program

--- Pi_Sensor_Script_Addition.py
+++ Pi_Sensor_Script_Addition.py
@@ -17,6 +17,7 @@ Structure: sensorData = array of doubles with length of 80 bytes
 
 # line ~38 ----------------
 # Data file names - NOTE: Change N & X each run
+global file1_name, file2_name, f1, f2
 file1_name = 'VICON_Data_Run_N-Car_Number_X.txt'
 file2_name = 'Sensor_Data_Run_N-Car_Number_X.txt'
 
@@ -34,40 +35,88 @@ saveData(f2, 'Time', ['Heading Velocity', 'Acceleration 1', 'Acceleration 2', 'A
 
 ''' ----- Running variables: should I put them in __init__ (~line 117) & change them to self.variable or take the already defined variables out? --------- '''
 # sensor and vicon data variables
-vX = 0.0; vY = 0.0; vTheta = 0.0; vThetaDot = 0.0; vSpeed = 0.0; vTime = rospy.get_time() #speed is just to check later
-sVelo = 0.0; sAccel0 = 0.0; sAccel1 = 0.0; sAccel2 = 0.0; sMag0 = 0.0; sMag1 = 0.0; sMag2 = 0.0; sGyro0 = 0.0; sGyro1 = 0.0; sGyro2 = 0.0; sTime = rospy.get_time(); 
+                # Running variables
+		# vicon and sensor data variables
+		self.vX = 0.0 	#VICON Variables
+		self.vY = 0.0 
+		self.vTheta = 0.0 
+		self.vThetaDot = 0.0 
+		self.vSpeed = 0.0 
+		self.vTime = rospy.get_time() 
+		#speed is just to check later
 
-# for old data variables
-o_vX = 0.0; o_vY = 0.0; o_vTheta = 0.0; o_vThetaDot = 0.0; o_vSpeed = 0.0; o_vTime = rospy.get_time() 
-o_sVelo = 0.0; o_sAccel0 = 0.0; o_sAccel1 = 0.0; o_sAccel2 = 0.0; o_sMag0 = 0.0; o_sMag1 = 0.0; o_sMag2 = 0.0; o_sGyro0 = 0.0; o_sGyro1 = 0.0; o_sGyro2 = 0.0; o_sTime = rospy.get_time()
+		self.sVelo = 0.0 	#Sensor Variables
+		self.sAccel0 = 0.0 
+		self.sAccel1 = 0.0 
+		self.sAccel2 = 0.0 
+		self.sMag0 = 0.0 
+		self.sMag1 = 0.0 
+		self.sMag2 = 0.0 
+		self.sGyro0 = 0.0 
+		self.sGyro1 = 0.0 
+		self.sGyro2 = 0.0 
+		self.sTime = rospy.get_time(); 
 
-# comparison vectors (time not included in state)
-ViconState = [ vX, vY, vTheta, vThetaDot, vSpeed ]
-sensorState = [ sVelo, sAccel0, sAccel1, sAccel2, sMag0, sMag1, sMag2, sGyro0, sGyro0, sGyro2 ]
+		# for old data variables
+		self.o_vX = 0.0 
+		self.o_vY = 0.0 
+		self.o_vTheta = 0.0 
+		self.o_vThetaDot = 0.0 
+		self.o_vSpeed = 0.0 
+		self.o_vTime = rospy.get_time()
+ 
+		self.o_sVelo = 0.0 
+		self.o_sAccel0 = 0.0 
+		self.o_sAccel1 = 0.0 
+		self.o_sAccel2 = 0.0 
+		self.o_sMag0 = 0.0 
+		self.o_sMag1 = 0.0 
+		self.o_sMag2 = 0.0 
+		self.o_sGyro0 = 0.0 
+		self.o_sGyro1 = 0.0 
+		self.o_sGyro2 = 0.0 
+		self.o_sTime = rospy.get_time()
 
-oldViconState =  [ o_vX, o_vY, o_vTheta, o_vThetaDot, o_vSpeed ]
-oldSensorState = [ o_sVelo, o_sAccel0, o_sAccel1, o_sAccel2, o_sMag0, o_sMag1, o_sMag2, o_sGyro0, o_sGyro1, o_sGyro2 ]
+		# comparison vectors (time not included in state vector)
+		self.ViconState = [ vX, vY, vTheta, vThetaDot, vSpeed ]
+		self.sensorState = [ sVelo, sAccel0, sAccel1, sAccel2, sMag0, sMag1, sMag2, sGyro0, sGyro0, sGyro2 ]
+
+		self.oldViconState =  [ o_vX, o_vY, o_vTheta, o_vThetaDot, o_vSpeed ]
+		self.oldSensorState = [ o_sVelo, o_sAccel0, o_sAccel1, o_sAccel2, o_sMag0, o_sMag1, o_sMag2, o_sGyro0, o_sGyro1, o_sGyro2 ]
 
 # In Update ~line 460 (used inside a loop) --------------------------------------------------
 # probably don't need new variables?
-vX = self.pos[0][0]; vY = self.pos[0][1]; vTheta = self.pos[1][2]; 
-vThetaDot = self.yaw_dot; vSpeed = self.velocity_current; vTime = rospy.get_time()
+		self.vX = self.pos[0][0]
+		self.vY = self.pos[0][1]
+		self.vTheta = self.pos[1][2] 
+		self.vThetaDot = self.yaw_dot
+		self.vSpeed = self.velocity_current 
+		self.vTime = rospy.get_time()
 
-# I may need to export the data from the arduino as a string instead of an array of doubles
-sVelo = self.ser.read(8); sAccel0 = self.ser.read(8); sAccel1 = self.ser.read(8); sAccel2 = self.ser.read(8); sMag0 = self.ser.read(8); 
-sMag1 = self.ser.read(8); sMag2 = self.ser.read(8); sGyro0 = self.ser.read(8); sGyro1 = self.ser.read(8); sGyro2 = self.ser.read(8);
+		# I may need to export the data from the arduino as a string instead of an array of doubles
+		self.sVelo = self.ser.read(8)
+		self.sAccel0 = self.ser.read(8)
+		self.sAccel1 = self.ser.read(8)
+		self.sAccel2 = self.ser.read(8)
+		self.sMag0 = self.ser.read(8) 
+		self.sMag1 = self.ser.read(8)
+		self.sMag2 = self.ser.read(8)
+		self.sGyro0 = self.ser.read(8)
+		self.sGyro1 = self.ser.read(8)
+		self.sGyro2 = self.ser.read(8)
+		self.sTime = rospy.get_time()
 
-ViconState = [ vX, vY, vTheta, vThetaDot, vSpeed ]
-sensorState = [ sVelo, sAccel0, sAccel1, sAccel2, sMag0, sMag1, sMag2, sGyro0, sGyro1, sGyro2 ]
+		self.ViconState = [ self.vX, self.vY, self.vTheta, self.vThetaDot, self.vSpeed ]
+		self.sensorState = [ self.sVelo, self.sAccel0, self.sAccel1, self.sAccel2, self.sMag0, 
+		self.sMag1, self.sMag2, self.sGyro0, self.sGyro1, self.sGyro2 ]
         
-if not (np.allclose(ViconState, oldViconState)):
-        saveData(f1, vTime, ViconState)
-        oldViconState = ViconState; o_vTime = vTime
+		if not (np.allclose(ViconState, oldViconState)):
+        		saveData(f1, vTime, ViconState)
+        		oldViconState = ViconState; o_vTime = vTime
 
-if not (np.allclose(sensorState, oldSensorState)):
-        saveData(f2, sTime, sensorState)
-        oldSensorState = sensorState; o_sTime = sTime
-
+		if not (np.allclose(sensorState, oldSensorState)):
+        		saveData(f2, sTime, sensorState)
+        		oldSensorState = sensorState; o_sTime = sTime
 # in shutdown before exit() ~line 625 -----------------------------
 f1.close(); f2.close()
 
