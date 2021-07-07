197af9fadf738709fa0836d1af56b5a7d8acc60f
Frank Doyle <fdoyle@udel.edu>
2021-07-07 10:21:41 -0400

    add filenames and file opening

--- Variance_Calculator.py
+++ Variance_Calculator.py
@@ -2,3 +2,11 @@ import numpy as np
 from matplotlib import pyplot as plt
 import scipy as sp
 
+runNumber = ''
+carNumber = ''
+
+viconFileName = 'VICON_Data_Run_' + runNumber + '-Car_Number_' + carNumber + '.txt'
+sensorFileName = 'Sensor_Data_Run_' + runNumber + '-Car_Number_' + carNumber + '.txt'
+
+viconFile = open(viconFileName, 'r')
+sensorFile = open(sensorFileName, 'r')
